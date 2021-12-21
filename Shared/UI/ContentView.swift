be5429e970f3099018c85de3019c620f4ce2b1e9
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-12-21 10:56:41 +0300

    Created MoviesVM

--- Shared/UI/ContentView.swift
+++ Shared/UI/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Favorites")
+                .tabItem{ Text("Favorites") }
+            Text("Browse")
+                .tabItem{ Text("Browse") }
+            Text("Settings")
+                .tabItem{ Text("Settings") }
+        }
     }
 }
 
---
+++ Shared/ViewModels/MoviesVM.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesVM.swift
+//  OTUS_Homework_8
+//
+//  Created by Вячеслав Погорельский on 21.12.2021.
+//
+
+import Foundation
+
+
+class MoviesViewModel: ObservableObject {
+    
+    private let moviesApi
+    
+    
+}
